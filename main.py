f5db45accfd49c215df59003f2fd48e1b73d2b41
samuel-sjoden <sjodensamuel@gmail.com>
2025-02-23 17:10:08 -0800

    Executable

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import socket
+
 from local_server import start_service
 
 
 def main():
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    print("Hostname:  ", host_name)
+    print("Host IP: ", host_ip)
     feeding_rate_path = input(f"Enter the path to the excel feeding rate workbook that will be used for "
                               f"data entry on this hot test: ")
     start_service(feeding_rate_path)
